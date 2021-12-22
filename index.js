add6571d36b91c0e2c29c3aa01c125e3c113b8dc
Pat Laplante <laplante.patrick@gmail.com>
2021-12-21 22:46:20 -0500

    Added pull request action & fix a typo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const aws = require("@pulumi/aws");
 const awsx = require("@pulumi/awsx");
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket(coc-rotter");
+const bucket = new aws.s3.Bucket("coc-rotter");
 
 // Export the name of the bucket
 exports.bucketName = bucket.id;
